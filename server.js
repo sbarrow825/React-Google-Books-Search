1a1673e6d4d491aed2c7b475c99c6f383a8dd4ff
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-25 19:22:34 -0700

    changed package.json to react script

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -12,11 +12,16 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksdb",
+{useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
+
+app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {

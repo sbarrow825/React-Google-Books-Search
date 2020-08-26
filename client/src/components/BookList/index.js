30b859b852eaab07eba69507d753543ef04ca0ea
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-25 19:00:52 -0700

    added BookList component

---
+++ client/src/components/BookList/index.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { useBookContext } from "../../utils/GlobalState"
+import Col from "../Col"
+import Row from "../Row"
+import { SearchButtons, SavedButtons } from "../Buttons"
+
+function BookListItem({ book }) {
+
+    if (window.location.pathname === "/saved") {
+        if (book.description) {
+            return (
+                <Col columns="col-sm-6 col-md-6 col-lg-6 p-4">
+                    <Row>
+                        <Col columns="col-sm-8 col-md-8 col-lg-8 book-item">
+                            <p className="text-center mt-3">{book.title}</p>
+                            <p className="text-center">{book.authors.join(",  ")}</p>
+                            {window.location.pathname === "/saved" ? <SavedButtons book={book} /> : <SearchButtons book={book} />}
+                        </Col>
+                        <Col columns="col-sm-4 col-md-4 col-lg-4 book-item">
+                            <img className="list-img" src={book.src} />
+
+                        </Col>
+                    </Row>
+                </Col>
+            )
+
+        }
+        else {
+            return null
+        }
+    }
+
+    else {
+        if (book.description) {
+            return (
+                <Col columns="col-sm-6 col-md-6 col-lg-6 p-4">
+                    <Row>
+                        <Col columns="col-sm-8 col-md-8 col-lg-8 book-item">
+                            <p className="text-center mt-3">{book.title}</p>
+                            <p className="text-center">{book.authors.join(",  ")}</p>
+                            {window.location.pathname === "/saved" ? <SavedButtons book={book} /> : <SearchButtons book={book} />}
+                        </Col>
+                        {book.imageLinks.smallThumbnail &&
+                        <Col columns="col-sm-4 col-md-4 col-lg-4 book-item">
+                            <img className="list-img" src={book.imageLinks.smallThumbnail} />
+
+                        </Col>}
+                    </Row>
+                </Col>
+            )
+
+        }
+        else {
+            return null
+        }
+    }
+
+
+}
+
+function BookList() {
+
+    const [state, dispatch] = useBookContext()
+
+
+    if (window.location.pathname === "/saved") {
+
+        return (
+            <div className="mt-3">
+                
+                    {state.books.length > 0 ? <Row> {state.books.map(book => {
+                        return <BookListItem key={state.books.indexOf(book)} book={book} /> }
+                    )} </Row> : <p className="text-center title-text larger-text">You haven't saved any books yet!</p>}
+
+                
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="mt-3">
+                <Row>
+                    {state.searchResults ? state.searchResults.map(book => {
+                        return <BookListItem book={book} />
+                    }) : ""}
+                </Row>
+            </div>
+        )
+    }
+
+
+
+
+}
+
+export default BookList

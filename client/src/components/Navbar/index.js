94c4a6a71d7ec3afa6d6acc6c4791b32d0211357
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-25 18:34:15 -0700

    added Navbar component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useBookContext } from "../../utils/GlobalState"
+import { UPDATE_SEARCH_RESULTS } from "../../utils/actions"
+
+function Navbar() {
+
+    const [state, dispatch] = useBookContext()
+
+    const clearSearches = () => {
+        dispatch({type: UPDATE_SEARCH_RESULTS, searchResults: null})
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand title-text" href="#">Google Books with React</a>
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a href="/search" className="nav-link font-weight-bold title-text" onClick={clearSearches}>Search for Books</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a href="/saved" className="nav-link font-weight-bold title-text" onClick={clearSearches}>Saved Books</a>
+                    </li>
+                </ul>
+        </nav>
+    )
+}
